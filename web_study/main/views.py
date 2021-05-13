32d810eacff4fe41a79915292625c513b975a1f3
lee jungwoo <ljw69700@gmail.com>
2021-05-14 06:44:45 +0900

    update category

--- web_study/main/views.py
+++ web_study/main/views.py
@@ -5,8 +5,8 @@ import os
 def index(request):
     return render(request, 'main/index.html')
 
-def modelview(request):
-    path="./media/models"
+def modelview(request, category):
+    path = "./media/models/"+category+'/'
     model_list = os.listdir(path)
     model_list.sort()
     return render_to_response('main/model_view.html', {'list': model_list})
