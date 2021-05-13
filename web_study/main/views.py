193ad5270d03cce9bff747f8f217620d9f75f810
jng-u <ljw69700@gmail.com>
2021-05-13 23:20:13 +0000

    commit

--- web_study/main/views.py
+++ web_study/main/views.py
@@ -5,8 +5,11 @@ import os
 def index(request):
     return render(request, 'main/index.html')
 
-def modelview(request, category):
+def modelview(request):
+    # path = "./media/models"
+    category = request.GET.get('category' , None)
     path = "./media/models/"+category+'/'
+
     model_list = os.listdir(path)
     model_list.sort()
-    return render_to_response('main/model_view.html', {'list': model_list})
+    return render_to_response('main/model_view.html', {'category':category, 'list': model_list})
