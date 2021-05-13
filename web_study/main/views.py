f47eac0744770cf63cfdd9b297fb7ac7e7e5d7af
jng-u <ljw69700@gmail.com>
2021-05-13 08:17:14 +0000

    select 기능 추가

--- web_study/main/views.py
+++ web_study/main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+import os 
 
 # Create your views here.
 def index(request):
-    return render(request,'main/index.html')
+    path="./media/models" 
+    model_list = os.listdir(path)
+    model_list.sort()
+    return render_to_response('main/index.html', {'list': model_list})
+    # return render(request,'main/index.html', {'images': img_list})
